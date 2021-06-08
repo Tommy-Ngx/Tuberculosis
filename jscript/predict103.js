d7a49666e82780797be523a6d630fb5271ea8e19
Tommy NgX <58539340+Tommy-Ngx@users.noreply.github.com>
2021-06-09 02:53:12 +1000

    Update predict103.js

--- jscript/predict103.js
+++ jscript/predict103.js
@@ -46,7 +46,7 @@ let model;
 (async function () {
 	
 	model = await tf.loadModel('https://raw.githubusercontent.com/Tommy-Ngx/Tuberculosis/main/model_tb_1/model.json');
-	$("#selected-image").attr("src", "https://github.com/Tommy-Ngx/Tuberculosis/blob/main/assets/tb.jpg")
+	$("#selected-image").attr("src", "https://raw.githubusercontent.com/Tommy-Ngx/Tuberculosis/main/assets/tb.jpg")
 	
 	
 	
